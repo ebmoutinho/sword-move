909e02022e49dc41b1c3db9795b55d77692a8348
Elisa Moutinho <elisabmoutinho@gmail.com>
2025-12-18 14:49:23 +0000

    SH-015: Change grey-light color variable

--- src/styles/colors.styles.ts
+++ src/styles/colors.styles.ts
@@ -2,8 +2,7 @@ import { css } from "styled-components";
 
 export default css`
     --color-dark: #151515;
-    /* --color-grey-light: #EDEDED; */
-    --color-grey-light: #e8e2e2; //only for dev purposes
+    --color-grey-light: #ededed;
     --color-white: #ffffff;
     --color-blue-light: #5993ea;
     --color-blue-medium: #365d95;
